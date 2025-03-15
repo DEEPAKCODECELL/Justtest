21ff8645d86c8df6af25f5f4afa027324f11a516
DEEPAKCODECELL <IAMDEEPAK9608@GMAIL.COM>
2025-03-15 23:24:57 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import serviceReducer from './slices/serviceSlice';
 import BookingReducer from "./slices/bookingSlice";
+import ProviderReducer from "./slices/ProviderSlice"
 const store = configureStore({
   reducer: {
     user: userReducer,
     service: serviceReducer,
-    booking:BookingReducer
+    booking: BookingReducer,
+    provider: ProviderReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
