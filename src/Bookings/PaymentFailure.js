85e93103d02e80dabe6c8f6c74987e57aec6f31d
DEEPAKCODECELL <IAMDEEPAK9608@GMAIL.COM>
2025-03-15 23:33:25 +0530

    Create PaymentFailure.js

---
+++ src/Bookings/PaymentFailure.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import { View, Animated, Text } from "react-native";
+import { Check, Bike } from "lucide-react-native";
+import tw from "../../tailwind";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+const SuccessAnimation = () => {
+    const scaleAnim = new Animated.Value(0);
+
+    useEffect(() => {
+        Animated.timing(scaleAnim, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: true,
+        }).start();
+    }, []);
+
+    return (
+        <View style={tw`flex justify-center items-center`}>
+            <Animated.View
+                style={[
+                    tw`w-50 h-50 rounded-full bg-green-500 justify-center items-center`,
+                    { transform: [{ scale: scaleAnim }], opacity: scaleAnim },
+                ]}
+            >
+                <Check size={100} color="white" strokeWidth={6} />
+            </Animated.View>
+        </View>
+    );
+};
+
+const ScooterAnimation = () => {
+    const translateX = new Animated.Value(-150);
+
+    useEffect(() => {
+        Animated.timing(translateX, {
+            toValue: 10,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+    }, []);
+
+    return (
+        <Animated.View style={[tw`absolute bottom-30`, { transform: [{ translateX }] }]}>
+            <Bike size={200} color="black" />
+        </Animated.View>
+    );
+};
+
+const PaymentFailure = () => {
+    const navigation = useNavigation();
+    const route = useRoute();
+    const { BookingId } = route.params || {};
+
+    useEffect(() => {
+        const navigateTimer = setTimeout(() => {
+            navigation.navigate("MapComponent", { BookingId });
+        }, 3000); // Navigate after 3 sec
+
+        return () => clearTimeout(navigateTimer);
+    }, [navigation]);
+
+    return (
+        <View style={tw`flex-1 justify-center items-center bg-gray-100 p-6`}>
+            {/* Success Animation & Scooter Animation both start together */}
+            <SuccessAnimation />
+            <Text style={tw`text-4xl font-bold text-green-600 mt-6`}>failed To Process Paymnet</Text>
+            <Text style={tw`text-xl font-bold text-green-600 mt-6`}>If Money get Deducted From Your Account Then It get Returned In 24 Hours Working Hours</Text>
+        </View>
+    );
+};
+
+export default PaymentFailure;
